a9f48ad4e31925d24bafbd70d1868c64d57fd158
Yogita Sheth <yogitasheth@Yogitas-MacBook-Pro.local>
2018-04-25 08:57:52 -0700

    deleted redundant lines

--- server.js
+++ server.js
@@ -1,15 +1,7 @@
 const express = require('express');
-const morgan = require('morgan');
 const path = require('path');
 const app = express();
 const port = process.env.PORT || 3000;
-
-app.use(morgan('dev'));
-app.use(express.static(path.join(__dirname, 'public')));
-
-app.get('/', (req, res) => {
-  res.redirect('/restaurants/ChIJZR1IXIiAhYARVdRHncWNWRY');
-});
 
 app.use('/restaurants/:id', express.static(path.join(__dirname, 'public')));
 
